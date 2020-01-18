c13615c6608fc57cfa833dfd3d5016333c11a572
chenxingli1 <2847770753@qq.com>
2020-01-19 00:54:43 +0800

    eureka

--- hello-server/src/main/java/com/hello/HelloController.java
+++ hello-server/src/main/java/com/hello/HelloController.java
@@ -1,14 +1,27 @@
 package com.hello;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.apache.log4j.Logger;
+
 @RestController
 public class HelloController {
 
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient client;
     @RequestMapping(value = "hello", method = RequestMethod.GET)
     public String hello() {
+        ServiceInstance instance = client.getLocalServiceInstance();
+//        System.out.println("hello, host"+ instance.getHost()+ instance.getServiceId());
+        logger.info("hello, host"+ instance.getHost()+" "+ instance.getPort() +" "+ instance.getServiceId());
         return "hello world";
     }
+
 }
