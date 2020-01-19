cbdde52ac4e7dfd3d73e89a4edb352e3a08711ca
chen <2847770753@qq.com>
2020-01-19 17:50:23 +0800

    eureka,ribbon

--- server-eureka/src/main/java/com/scloud/eureka/EurekaApplication.java
+++ server-eureka/src/main/java/com/scloud/eureka/EurekaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Application {
+public class EurekaApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class).web(true).run(args);
+        new SpringApplicationBuilder(EurekaApplication.class).web(true).run(args);
     }
 }
